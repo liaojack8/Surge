9c6a97eeb663f02edcee8244084f06a11891f5c7
LJack.SY <liaojack8.tw@gmail.com>
2025-10-19 18:08:30 +0800

    rule w/o proxy

--- scripts/tg_cidr_list.py
+++ scripts/tg_cidr_list.py
@@ -7,9 +7,9 @@ cidrs = response.text.strip().split("\n")
 rules = []
 for line in cidrs:
     if ":" in line:
-        rules.append(f"IP-CIDR6,{line},Proxy")
+        rules.append(f"IP-CIDR6,{line}")
     else:
-        rules.append(f"IP-CIDR,{line},Proxy")
+        rules.append(f"IP-CIDR,{line}")
 
 with open("rules/tg.list", "w") as f:
     f.write("\n".join(rules))
